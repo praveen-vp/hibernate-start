e0ddb7712cc83b6caf0b90943188d7a6a656a5d1
praveen-vp <praveen-vp@localhost>
2018-05-12 14:30:49 +0530

    Adding changes foe App.java

--- src/main/java/com/pvp/enc/hibernate/App.java
+++ src/main/java/com/pvp/enc/hibernate/App.java
@@ -1,13 +1,34 @@
 package com.pvp.enc.hibernate;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.pvp.enc.employee.Employee;
+
 /**
- * Hello world!
- *
+ * Employee
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	public static void main(String[] args) {
+
+		Employee emp = new Employee();
+		emp.setEmpId(100);
+		emp.setName("Praveen");
+		emp.setDepartment("java");
+		emp.setJob("programmer");
+
+		System.out.println(emp.toString());
+
+		Configuration conf = new Configuration().configure().addAnnotatedClass(Employee.class);
+
+		SessionFactory sf = conf.buildSessionFactory();
+		Session session = sf.openSession();
+		org.hibernate.Transaction tx = session.beginTransaction();
+
+		session.save(emp);
+		tx.commit();
+	}
+
 }
